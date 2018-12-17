52abf61082a1ff9884ac1903c92997df68e5a28b
SKC <test@fake.com>
2018-12-16 21:29:41 -0800

    Cleaned up main.py

--- main.py
+++ main.py
@@ -13,16 +13,7 @@ def get_templates(template_abs_path):
     logging.debug(template_entities)
     return [template_file for template_file in template_entities if os.path.isfile(template_file) and template_file.endswith("jinja2")]
 
-def render_with_template(yaml_data, template_path, template_id, target, output_dir):
-    format_ext = (os.path.splitext(os.path.splitext(template_id)[0])[-1]).lower()
-    target_name = target + format_ext
-    target_path = os.path.join(output_dir, target_name)
-    if not os.path.exists(output_dir):
-        logging.debug("Creating {}".format(output_dir))
-        os.makedirs(output_dir)
-
-    logging.debug("Rendering with {} to {}".format(template_id, target_path))
-
+def create_environment(template_path, format_ext):
     jinja2_template_loader = jinja2.FileSystemLoader(template_path)
     options = { "loader": jinja2_template_loader }
     if format_ext == ".tex":
@@ -32,7 +23,20 @@ def render_with_template(yaml_data, template_path, template_id, target, output_d
         options["variable_end_string"] = '>>'
         options["comment_start_string"] = '<#'
         options["comment_end_string"] = '#>'
-    jinja2_environment = jinja2.Environment(**options)
+    return jinja2.Environment(**options)
+
+def render_with_template(yaml_data, template_path, template_id, output_dir):
+    template_id_parts = template_id.split('.')
+    if len(template_id_parts) < 3 or template_id_parts[2].lower() != "jinja2":
+        logging.warning("template_id {} is not valid!".format(template_id))
+        return
+
+    target_name = os.path.splitext(template_id)[0]
+    format_ext = os.path.splitext(target_name)[-1].lower()
+    target_path = os.path.join(output_dir, target_name)
+
+    logging.debug("Rendering with {} to {}".format(template_id, target_path))
+    jinja2_environment = create_environment(template_path, format_ext)
     template = jinja2_environment.get_template(template_id)
     rendered_output = template.render(resume = yaml_data).encode('utf-8')
     with open(target_path, 'w') as output:
@@ -45,8 +49,13 @@ def parse_arguments():
     parser.add_argument('--input', '-i', dest='input_file', required=True, help='input yaml file for resume')
     parser.add_argument('--output', '-o',  dest='output_dir', default="products", help='output directory for formatted resumes')
     parser.add_argument('--template', '-t', dest='template_dir', required=True, help='directory of templates')
+    arguments = parser.parse_args()
 
-    return parser.parse_args()
+    # Normalize paths.
+    arguments.input_file = path_normalize(arguments.input_file)
+    arguments.output_dir = path_normalize(arguments.output_dir)
+    arguments.template_dir = path_normalize(arguments.template_dir)
+    return arguments
 
 def setup_logging():
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s, %(module)s, %(funcName)s, %(message)s")
@@ -54,13 +63,21 @@ def setup_logging():
 def main():
     setup_logging()
     args = parse_arguments()
-    template_files = get_templates(path_normalize(args.template_dir))
+
+    # Create output path
+    if not os.path.exists(args.output_dir):
+        logging.debug("Creating {}".format(args.output_dir))
+        os.makedirs(args.output_dir)
+
+    # Get list of valid jinja2 template files.
+    template_files = get_templates(args.template_dir)
     logging.debug(template_files)
-    with open(path_normalize(args.input_file), 'r') as yaml_input:
+
+    with open(args.input_file, 'r') as yaml_input:
         yaml_data = yaml.safe_load(yaml_input)
         for template_file in template_files:
-            render_with_template(yaml_data, path_normalize(args.template_dir),
-                os.path.basename(template_file), "resume", path_normalize(args.output_dir))
+            render_with_template(yaml_data, args.template_dir,
+                os.path.basename(template_file), args.output_dir)
 
 if __name__ == '__main__':
     main()
